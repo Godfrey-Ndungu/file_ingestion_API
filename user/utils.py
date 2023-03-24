ca68833fcf68e5a637e1a65cbf68f227a6ee37f0
Godfrey-Ndungu <godmak2022@gmail.com>
2023-03-24 23:31:39 +0300

    Add FileHeaderValidator utility class for validating file headers
    his commit introduces a new utility class called FileHeaderValidator that
    provides a method for validating the headers of a CSV file. The is_valid
    method accepts a file object as its argument and checks if the headers in the
    file match a predefined set of valid headers. If the headers in the file are
    valid, the method returns True, otherwise it returns False.

--- user/utils.py
+++ user/utils.py
@@ -5,3 +5,14 @@ class FileExtensionValidator:
     def is_csv(filename):
         """Validates if the file extension is CSV."""
         return filename.endswith(".csv")
+
+class FileHeaderValidator:
+    """Utility class for validating file headers."""
+    
+    @staticmethod
+    def is_valid(file_obj):
+        """Validates if the headers in a file are valid."""
+        valid_headers = ['first_name', 'last_name', 'national_id', 'birth_date', 'address', 'country', 'phone_number', 'email', 'finger_print_signature']
+        with open(file_obj, 'r') as f:
+            headers = f.readline().strip().split(',')
+            return set(valid_headers) == set(headers)
