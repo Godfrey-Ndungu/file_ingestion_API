caec561e9bfcc8d12f4b2d48a21bf277a15788c7
Godfrey-Ndungu <godmak2022@gmail.com>
2023-03-25 01:43:02 +0300

    feat: Add FileUploadViewSet and CSV file upload functionality

--- user/views.py
+++ user/views.py
@@ -1,10 +1,15 @@
-from rest_framework import pagination
+from rest_framework.parsers import FormParser, MultiPartParser
+from django.db import transaction
+from rest_framework.response import Response
+from rest_framework import status
 from rest_framework import filters
-from rest_framework.viewsets import GenericViewSet
+from rest_framework import pagination
+from rest_framework.viewsets import GenericViewSet, ModelViewSet
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.mixins import ListModelMixin
-from .models import UserData
-from .serializers import UserDataSerializer
+from .models import UserData, FileUpload
+from .serializers import UserDataSerializer, FileUploadSerializer
+from .utils import FileExtensionValidator
 
 
 class UserPagination(pagination.PageNumberPagination):
@@ -43,3 +48,29 @@ class UserDataViewSet(ListModelMixin, GenericViewSet):
                 birth_date__range=[start_date, end_date])
 
         return queryset
+
+
+class FileUploadViewSet(ModelViewSet):
+    parser_classes = [MultiPartParser, FormParser]
+    serializer_class = FileUploadSerializer
+    queryset = FileUpload.objects.all()
+
+    def create(self, request):
+        file_obj = request.FILES.get("file")
+        extension_validator = FileExtensionValidator()
+
+        # Validate file extension
+        if not extension_validator.is_csv(file_obj.name):
+            return Response(
+                {"error": "Invalid file type. Only CSV files are allowed."},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+        # Save file upload object
+        with transaction.atomic():
+            file_upload = FileUpload(
+                file=file_obj, status=FileUpload.FILE_STATUS_PENDING
+            )
+            file_upload.save()
+
+        return Response(FileUploadSerializer(file_upload).data)
