4e38a7dd104a8225c1ec683d0e85aeaa7952135f
Godfrey-Ndungu <godmak2022@gmail.com>
2023-03-25 06:48:49 +0300

    Fix post-save signal for file upload model

---
+++ user/signals.py
@@ -0,0 +1,10 @@
+from django.dispatch import receiver
+from django.db.models.signals import post_save
+from .models import FileUpload
+from .tasks import process_uploaded_file
+
+
+@receiver(post_save, sender=FileUpload)
+def start_file_upload_signal(sender, instance, created, **kwargs):
+    if created:
+        process_uploaded_file(instance.id)
