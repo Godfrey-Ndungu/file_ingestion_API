275c8673b8f9d5a5bba1cc3ad8226a78eaaa370b
Godfrey-Ndungu <godmak2022@gmail.com>
2023-03-25 06:49:55 +0300

    Fix project name in celery.py file

--- fileUpload/celery.py
+++ fileUpload/celery.py
@@ -1,7 +1,7 @@
 import os
 from celery import Celery
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_celery.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "fileUpload.development")
 app = Celery("django_celery")
 app.config_from_object("django.conf:settings", namespace="CELERY")
 app.autodiscover_tasks()
