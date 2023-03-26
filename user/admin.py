e9b6c6e5532373387da849f1a9287648fc5ba170
Godfrey-Ndungu <godmak2022@gmail.com>
2023-03-26 18:58:58 +0300

    Refactor user admin using black

--- user/admin.py
+++ user/admin.py
@@ -1,15 +1,33 @@
 from django.contrib import admin
 from .models import UserData, FileUpload
 
+
 @admin.register(UserData)
 class UserDataAdmin(admin.ModelAdmin):
-    list_display = ('id', 'first_name', 'last_name', 'national_id', 'birth_date', 'address', 'country', 'phone_number', 'email', 'finger_print_signature')
-    search_fields = ('first_name', 'last_name', 'national_id', 'email', 'finger_print_signature')
-    list_filter = ('country', 'time_added')
+    list_display = (
+        "id",
+        "first_name",
+        "last_name",
+        "national_id",
+        "birth_date",
+        "address",
+        "country",
+        "phone_number",
+        "email",
+        "finger_print_signature",
+    )
+    search_fields = (
+        "first_name",
+        "last_name",
+        "national_id",
+        "email",
+        "finger_print_signature",
+    )
+    list_filter = ("country", "time_added")
+
 
 @admin.register(FileUpload)
 class FileUploadAdmin(admin.ModelAdmin):
-    list_display = ('id', 'filename', 'status', 'time_added')
-    list_filter = ('status', 'time_added')
-    search_fields = ('file__name',)
-
+    list_display = ("id", "filename", "status", "time_added")
+    list_filter = ("status", "time_added")
+    search_fields = ("file__name",)
